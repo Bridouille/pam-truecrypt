175396f598a9a982d056eef2140c7adb490143bb
Nicolas Bridoux <nicolas.bridoux@epitech.eu>
2014-11-12 19:33:32 +0100

    added conf file and auto creating volume

--- module.c
+++ module.c
@@ -40,24 +40,33 @@ pam_sm_acct_mgmt(UNUSED pam_handle_t *pamh, UNUSED int flags,
   return (PAM_SUCCESS);
 }
 
+static char	*gen_path(char *user_name, char *volume_name)
+{
+  char		*path;
+
+  if (!volume_name || !user_name || !(path = malloc(sizeof(char) *
+     (strlen(user_name) + strlen(volume_name) + 8))))
+    return (NULL);
+  strcpy(path, "/home/");
+  strcat(path, user_name);
+  strcat(path, "/");
+  strcat(path, volume_name);
+  return (path);
+}
+
 static int	encrypt_volume(struct passwd *pwd,
-			       const char *volume_name, char *password)
+			       char *volume_name, char *password)
 {
   pid_t		pid;
   int		status;
   char		*path;
   char		*pass;
 
-  if (!(path = malloc(sizeof(char) *
-		      (strlen(pwd->pw_name) + strlen(volume_name) + 8))) ||
+  if (!(path = gen_path(pwd->pw_name, volume_name))||
       !(pass = malloc(sizeof(char) * (strlen(password) + 12)))) {
     warn("malloc()");
     return (PAM_SESSION_ERR);
   }
-  strcpy(path, "/home/");
-  strcat(path, pwd->pw_name);
-  strcat(path, "/");
-  strcat(path, volume_name);
   strcpy(pass, "--password=");
   strcat(pass, password);
   if ((pid = fork()) == -1) {
@@ -76,10 +85,66 @@ static int	encrypt_volume(struct passwd *pwd,
   waitpid(pid, &status, 0);
   free(path);
   free(password);
-  /* if (WEXITSTATUS(status))
-     return (PAM_SESSION_ERR); */
+  free(volume_name);
   return (PAM_SUCCESS);
+}
 
+static void	load_conf(char **volume_name, int *autocreate,
+			  struct passwd *pwd)
+{
+  char		*conf_file;
+  char		*line;
+  size_t	n;
+  FILE		*stream;
+
+  *volume_name = strdup(DEFAULT_VOLUME_NAME);
+  *autocreate = 0;
+  if (!(conf_file = malloc(sizeof(char) * (strlen("/home/") + strlen(pwd->pw_name) + strlen("/.my_modulerc") + 1))))
+    return (warn("malloc()"));
+  strcpy(conf_file, "/home/");
+  strcat(conf_file, pwd->pw_name);
+  strcat(conf_file, "/.my_modulerc");
+  if ((stream = fopen(conf_file, "r")) == NULL)
+    return (warn("fopen()"));
+  line = NULL;
+  while (getline(&line, &n, stream) != -1) {
+    line[strlen(line) - 1] = '\0';
+    if (!strncmp(line, "autocreate=", strlen("autocreate=")) &&
+	!strncmp(&line[strlen("autocreate=")], "true", 4))
+      *autocreate = 1;
+    if (!strncmp(line, "volume_name=", strlen("volume_name="))) {
+      free(*volume_name);
+      *volume_name = strdup(&line[strlen("volume_name=")]);
+    }
+    free(line);
+    line = NULL;
+  }
+  fclose(stream);
+}
+
+static int	create_volume(struct passwd *pwd)
+{
+  pid_t		pid;
+  int		status;
+
+  if ((pid = fork()) == -1) {
+    warn("fork()");
+    return (1);
+  }
+  if (!pid) {
+    if (setuid(pwd->pw_uid) == -1) {
+      warn("setuid()");
+      exit(PAM_SESSION_ERR);
+    }
+    execlp("truecrypt", "truecrypt", "-c", NULL);
+    warn("execlp()");
+    exit(PAM_SESSION_ERR);
+  }
+  if (waitpid(pid, &status, 0) == -1) {
+    warn("waitpid()");
+    return (1);
+  }
+  return (WEXITSTATUS(status));
 }
 
 PAM_EXTERN int
@@ -92,14 +157,37 @@ pam_sm_open_session(pam_handle_t *pamh, UNUSED int flags,
   struct pam_response		*resp;
   struct passwd			*pwd;
   const char			*user;
-  char				*password;
+  char				*volume_name;
+  char				*password = NULL;
+  char				*path;
   int				pam_err;
+  int				autocreate;
 
   if ((pam_err = pam_get_user(pamh, &user, NULL)) != PAM_SUCCESS)
     return (pam_err);
   if ((pwd = getpwnam(user)) == NULL)
     return (PAM_USER_UNKNOWN);
-
+  load_conf(&volume_name, &autocreate, pwd);
+  if (!(path = gen_path(pwd->pw_name, volume_name)))
+    return (PAM_SESSION_ERR);
+  printf("\n\n");
+  if (access(path, F_OK) == -1) {
+    printf("Volume at %s doesn't exist\n", path);
+    if (autocreate) {
+      printf("Creating volume...\n");
+      pam_err = create_volume(pwd);
+      printf("%s\n", pam_err ? "Well done, opening volume now..." :
+	     "Error while creating volume");
+      if (pam_err) {
+	free(path);
+	return (PAM_SESSION_ERR);
+      }
+    } else {
+      free(path);
+      return (PAM_SUCCESS);
+    }
+  }
+  free(path);
   pam_err = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
   if (pam_err != PAM_SUCCESS)
     return (PAM_SYSTEM_ERR);
@@ -116,11 +204,10 @@ pam_sm_open_session(pam_handle_t *pamh, UNUSED int flags,
     free(resp);
   }
   if (pam_err == PAM_CONV_ERR)
-    return (pam_err);
+    return (PAM_CONV_ERR);
   if (pam_err != PAM_SUCCESS)
     return (PAM_AUTH_ERR);
-  return (encrypt_volume(pwd, ac == 1 ?
-			 av[0] : DEFAULT_VOLUME_NAME, password));
+  return (encrypt_volume(pwd, volume_name, password));
 }
 
 PAM_EXTERN int
